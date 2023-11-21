21c3e41730ba1443b010f0ad17c625d47fb6e5b9
kwokv244 <kwokv244@newschool.edu>
2023-11-20 21:57:07 -0500

    entry10

--- entries/entry10/main.js
+++ entries/entry10/main.js
@@ -10,3 +10,22 @@ function turnOnLight() {
 
 let button = document.querySelector(".button");
 button.addEventListener("click", turnOnLight);
+
+const cursor = document.getElementById('cursor');
+
+document.addEventListener('mousemove', (e) => {
+  const x = e.clientX - 8; 
+  const y = e.clientY - 8; 
+
+  cursor.style.left = x + 'px';
+  cursor.style.top = y + 'px';
+});
+
+function toggleImage() {
+  var image = document.getElementById('image');
+
+  var isVisible = window.getComputedStyle(image).display !== 'none';
+  image.style.display = isVisible ? 'none' : 'block';
+}
+
+
